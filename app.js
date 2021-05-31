30f852971cf921481d8d28ae0cac9263ed791ce9
sohakhan77 <sohakhank73@gmail.com>
2021-05-31 08:20:10 -0700

    work complete

---
+++ app.js
@@ -0,0 +1,7 @@
+// var firstName = ["Shabana","Urooj","Sharjeel","Faizan"];
+// var lastName = ["Roshan","Khan","Juniad","Malik"];
+// for(var i = 0; i < firstName.length; i++){
+//   for(var j = 0; j < lastName.length; j++){
+//     document.write(firstName[i]+' '+lastName[j]+"<br/>");
+//   }
+// }
